1b55bf6a8d75383d25bef1ede595c28503202186
brandonerose <brandon.ed.rose@gmail.com>
2024-08-27 10:01:34 -0400

    dev

--- inst/files/dev.R
+++ inst/files/dev.R
@@ -2,6 +2,8 @@
 # remotes::install_github("brandonerose/Rosyverse")
 # Rosyverse::update_all()
 # Rosyverse::load_all()
+# RosyUtils::clear_env
+# main =========================================================================
 (pkg_dir <- getwd())
 (pkg_name <- basename(pkg_dir))
 # devtools::load_all()
@@ -10,22 +12,27 @@ RosyDev::setup_RosyDev(overwrite = T) # be careful will overwrite certain files
 
 RosyDev::dev_update()
 
-usethis::use_version(which = "dev")
+RosyDev::fast_commit(
+  message = "dev",
+  push = T,
+  ask = T,
+  bump_version = T,
+  which = "dev"
+)
 
-RosyDev::dev_update()
+#other =========================================================================
 
 devtools::build_readme()
 
-RosyDev::fast_commit(message = "dev", push = T, bump_version = T, which = "dev")
-
-system("git push")
+usethis::use_version(which = "dev")
 
 devtools::check()
 
 devtools::build()
 
-#open files -----
+#open files ====================================================================
 
+rstudioapi::filesPaneNavigate(getwd())
 RosyDev::pkg_dev_nav_combined()
 RosyDev::pkg_dev_nav_dev()
 RosyDev::pkg_dev_nav_setup()
